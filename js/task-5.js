77c1d556e7fa6451ba9b079a9a0774519435d250
Aleksandr Chudak <63600579+AlexChudak@users.noreply.github.com>
2020-12-21 11:11:11 +0200

    Re-submit

--- js/task-5.js
+++ js/task-5.js
@@ -4,45 +4,43 @@ let answerTlc;
 
 const answer = prompt('введите название страны для доставки');
 
-if (answer === null) {
-} else {
+if (answer !== null) {
   answerTlc = answer.toLowerCase();
 
   switch (answerTlc) {
     case 'китай': {
       country = 'Китай';
       price = '100';
-      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     }
     case 'чили': {
       country = 'Чили';
       price = '250';
-      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     }
     case 'австралия': {
       country = 'Австралия';
       price = '170';
-      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     }
     case 'индия': {
       country = 'Индия';
       price = '80';
-      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     }
     case 'ямайка': {
       country = 'Ямайка';
       price = '120';
-      alert(`Доставка в ${country} будет стоить ${price} кредитов`);
       break;
     }
 
     default:
       alert('В вашей стране доставка не доступна');
   }
+}
+
+if (price) {
+  alert(`Доставка в ${country} будет стоить ${price} кредитов`);
 }
 
 //alert(`Доставка в ${country} будет стоить ${price} кредитов`);
